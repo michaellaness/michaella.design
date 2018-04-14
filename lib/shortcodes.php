aa33ec4ab15ed504d6d818db158dbec7ad800c9a
Chris Holder <chrisholder@Chriss-Mac-mini.local>
2018-04-14 14:48:34 -0600

    row and column shortcodes

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -1,3 +1,27 @@
 <?php
 
-// Shortcodes
+// ---- Shortcodes ---- //
+
+
+
+
+// Rows
+function row_shortcode($atts, $content = null ) {
+
+   $output= '<div class="row-shortcode">'.do_shortcode($content).'</div>';
+
+   return $output;
+}
+
+// Columns
+function column_shortcode($atts, $content = null) {
+   extract(shortcode_atts(array(
+      'size' => '',
+      'headline' => '',
+      'alignment' => 'left',
+   ), $atts));
+
+   $output = '<div class="column '.$size.' txt-'.$alignment.'"><h3>'.do_shortcode($headline).'</h3><p>'.do_shortcode($content).'</p></div>';
+
+   return $output;
+}
